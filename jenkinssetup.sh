3130ebe0ff41fb6ef71574e6dddf90f8105bc0e1
Amit Kumar Sharma <amit.sharma@publicissapient.com>
2020-08-10 17:55:42 -0500

    added git and kubectl installation

--- jenkinssetup.sh
+++ jenkinssetup.sh
@@ -6,3 +6,17 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins -y
 sudo service jenkins start
+
+
+sudo yum install git -y
+
+sudo -su jenkins
+
+sudo curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.16.12/2020-07-08/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+
+sudo mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+sudo echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+kubectl version --short --client
